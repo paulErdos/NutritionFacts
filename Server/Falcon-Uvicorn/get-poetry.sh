e97ecf43e7e21ba454e2aebb516e9cc3241aa317
NICK BUENO <tehbobum@gmail.com>
2025-08-07 23:00:41 -0700

    Broke ground on adding automated unit test framework

---
+++ Server/Falcon-Uvicorn/get-poetry.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pipx install poetry
+poetry completions bash >> ~/.bash_completion
---
+++ Server/Falcon-Uvicorn/tests/unit_tests.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class TestTheTests(untittest.TestCase)
+
+    def test_smoketest(self):
+        self.assertTrue(True)

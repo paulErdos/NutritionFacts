94bd94d07be7dbf6ffae03896baab885f9b5d711
vincent <vincesteffens@gmail.com>
2025-08-12 13:57:57 -0700

    The issue seems to be nginx

--- setup-git.sh
+++ setup-git.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-git update-index --assume-unchanged Server/Falcon-Uvicorn/src/nf_server/logs/*
+# * does nothing
+
 git update-index --assume-unchanged Server/Falcon-Uvicorn/src/nf_server/logs/err.txt
 git update-index --assume-unchanged Server/nginx/logs/nginx.pid
 git update-index --assume-unchanged Server/nginx/logs/error.log
+git update-index --assume-unchanged Server/nginx/logs/access.log
+

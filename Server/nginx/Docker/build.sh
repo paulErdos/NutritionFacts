d519e39db10503c8f9aeff2e08d27b5b93b3a88f
Vincent Steffens <vincesteffens@gmail.com>
2026-08-16 15:25:00 -0700

    Connect dockerized nginx to data server

--- Server/nginx/Docker/build.sh
+++ Server/nginx/Docker/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp -r ../Client .
+#cp -r ../Client .
                         # Increment / label builds nf-nginx:<here>
 docker build -t nf-nginx:latest .
 
--- Server/nginx/Docker/run.sh
+++ Server/nginx/Docker/run.sh
@@ -6,4 +6,10 @@
 # nf-nginx:latest: <image>:<tag>. If the tag is not found, docker attempts to pull
 # --rm: Deletes the container when it stops.
 echo 'Serving on port 8083'
-docker run --rm --name nf-nginx-container -p 8083:9003 nf-nginx:latest
+docker run --rm --name nf-nginx-container -p 8083:9003 -p 9001:9001 nf-nginx:latest 
+#docker run \
+#	--rm \
+#	--name nf-nginx-container \
+#	-p 8083:9003 \# serves on port 9003 --> connect to host port 8083 \
+#	-p 8081:9001 \# Data server on 8081 --> into port 9001 \
+#	nf-nginx:latest 
